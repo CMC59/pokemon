635fbd3d1e7e55c7c917effa85cbf27282261a70
Philippe Plaïa <philippeplaia@gmail.com>
2023-12-04 23:12:14 +0100

    Add sprite with evol links

--- pokemon.php
+++ pokemon.php
@@ -25,22 +25,26 @@ if (!empty($pokemonDetails)) {
         echo "<img src='{$type["image"]}' alt='{$type["name"]}' />";
     }
     echo "</p>";
-
-    // Afficher les évolutions
-    $evolutions = json_decode($pokemonDetails[0][7], true);
-    if (!empty($evolutions)) {
-        echo "<p>Evolutions: ";
-        foreach ($evolutions as $evolution) {
-            echo "<a href='pokemon.php?pokemon={$evolution["pokedexId"]}'>{$evolution["name"]}</a>, ";
-        }
-        echo "</p>";
+// Afficher les évolutions
+$evolutions = json_decode($pokemonDetails[0][7], true);
+if (!empty($evolutions)) {
+    echo "<p>Evolutions: ";
+    foreach ($evolutions as $evolution) {
+        $evolutionDetails = $dao->PokemonById($evolution["pokedexId"]);
+        echo "<a href='pokemon.php?pokemon={$evolutionDetails[0][1]}'>{$evolutionDetails[0][2]}";
+        echo "<img src='{$evolutionDetails[0][4]}' alt='{$evolutionDetails[0][2]}'></a>";
     }
+    echo "</p>";
+}
 
-    // Afficher la pré-évolution
-    $preEvolution = json_decode($pokemonDetails[0][8], true);
-    if (!empty($preEvolution) && $preEvolution != "none") {
-        echo "<p>Pre-evolution: <a href='pokemon.php?pokemon={$preEvolution["pokedexIdd"]}'>{$preEvolution["name"]}</a></p>";
-    }
+// Afficher la pré-évolution
+$preEvolution = json_decode($pokemonDetails[0][8], true);
+if (!empty($preEvolution) && $preEvolution != "none") {
+    $preEvolutionDetails = $dao->PokemonById($preEvolution["pokedexIdd"]);
+    echo "<p>Pre-evolution: <a href='pokemon.php?pokemon={$preEvolutionDetails[0][1]}'>{$preEvolutionDetails[0][2]}";
+    echo "<img src='{$preEvolutionDetails[0][4]}' alt='{$preEvolutionDetails[0][2]}'></a></p>";
+}
+
     
     // ... Autres détails
 } else {
